8d3fd3489e01ca44cac0fedebdba9eb995c856e4
eternaleight <eternaleight0@gmail.com>
2022-02-22 01:00:23 +0900

    twitter

--- client/components/home/TweetBox.js
+++ client/components/home/TweetBox.js
@@ -1,3 +1,8 @@
+import { useState } from "react"
+import { BsCardImage, BsEmojiSmile } from "react-icons/bs"
+import { RiBarChartHorizontalFill, RiFileGifLine } from "react-icons/ri"
+import { IoMdCalendar } from "react-icons/io"
+import { MdOutlineLocationOn } from "react-icons/md"
 
 
 const style = {
@@ -14,17 +19,51 @@ const style = {
   activeSubmit: `bg-[#1d9bf0] text-white`,
 }
 
-function Tweetbox() {
+const TweetBox = () => {
+  const [tweetMessage, setTweetMessage] = useState('')
+  const postTweet = (event) => {
+    console.log(tweetMessage)
+  }
+
   return (
     <div className={style.wrapper}>
       <div className={style.tweetBoxLeft}>
         <img
           src="https://avatars.githubusercontent.com/u/96198088?v=4"
           alt="profile image"
-          className={style.profileImage} />
+          className={style.profileImage}
+        />
+      </div>
+      <div className={style.tweetBoxRight}>
+        <form>
+          <textarea
+            className={style.inputField}
+            placeholder="What's happening"
+            value={tweetMessage}
+            onChange={(e) => setTweetMessage(e.target.value)}
+          />
+          <div className={style.formLowerContainer}>
+            <div className={style.iconsContainer}>
+              <BsCardImage className={style.icon} />
+              <RiFileGifLine className={style.icon} />
+              <RiBarChartHorizontalFill className={style.icon} />
+              <BsEmojiSmile className={style.icon} />
+              <IoMdCalendar className={style.icon} />
+              <MdOutlineLocationOn className={style.icon} />
+            </div>
+            <button
+              type='submit'
+              disabled={!tweetMessage}
+              onClick={(event) => postTweet(event)}
+              className={`${style.submitGeneral} ${tweetMessage ? style.activeSubmit : style.inactiveSubmit}`}
+            >
+              Tweet
+            </button>
+          </div>
+        </form>
       </div>
     </div>
   )
 }
 
-export default Tweetbox
+export default TweetBox

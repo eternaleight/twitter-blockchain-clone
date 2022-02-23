b4bb9d2dcc98bcf4817b7a4267689732b7ac0b61
eternaleight <eternaleight0@gmail.com>
2022-02-24 05:42:22 +0900

    twitter

--- client/pages/list.js
+++ client/pages/list.js
@@ -2,6 +2,7 @@ import * as React from 'react';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 import Transition from '../components/Transition'
+import Link from 'next/link'
 
 const style = {
   wrapper: `flex justify-center h-screen w-screen select-none bg-[#15202b] text-white`,
@@ -12,10 +13,15 @@ const style = {
 
 const list = () => {
   return (
-    <div className={`${style.wrapper} flex wrap justify-around flex-wrap`}>List
-      <img className={`${style.banjyo} cursor-pointer`} src='image-5.png' />
-      <Transition item='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1inLywmsyHAMuV9D1VdKW0PZsx9RawIayulqNLlvl-lYbhc5YLIy-8tsGlN0YQffrFL0&usqp=CAU' />
-      <Transition item='名称未設定211q.png' />
+    <div>
+      <Link href="/">
+        <div className={`w-screen flex wrap justify-around text-white bg-[#15202b] flex-wrap hover:bg-slate-700 cursor-pointer`}>home </div>
+      </Link>
+      <div className={`${style.wrapper} flex wrap justify-around flex-wrap`}>List
+        <img className={`${style.banjyo} cursor-pointer`} src='image-5.png' />
+        <Transition item='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1inLywmsyHAMuV9D1VdKW0PZsx9RawIayulqNLlvl-lYbhc5YLIy-8tsGlN0YQffrFL0&usqp=CAU' />
+        <Transition item='名称未設定211q.png' />
+      </div>
     </div>
   )
 }

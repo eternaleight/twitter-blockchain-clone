8ce17857b0a84055aed066382166a50bfa96a87c
eternaleight <eternaleight0@gmail.com>
2022-02-25 02:16:29 +0900

    twitter

--- client/pages/list.js
+++ client/pages/list.js
@@ -9,7 +9,7 @@ import Switch from '../components/ui/Switch'
 import DynamicCSS from '../components/ui/DynamicCSS'
 import IconButton from '@mui/material/IconButton';
 import Fingerprint from '@mui/icons-material/Fingerprint';
-
+import Sidebar from '../components/Sidebar'
 
 
 
@@ -18,7 +18,7 @@ const style = {
   wrapper: `flex  justify-center h-screen w-screen select-none bg-[#15202b] text-white`,
   content: `max-w-[1400px] w-2/3 flex justify-between`,
   mainContent: `flex-[2] border-r border-l border-[#38444d] overflow-y-scroll`,
-  banjyo: `max-w-[350px] max-h-[300px] p-[10px] w-[80%] pt-[50px] hover:opacity-[0.8]`,
+  banjyo: `max-w-[350px] max-h-[300px] p-[10px] w-[80%] pt-[50px] hover:opacity-[0.8] `,
 }
 
 const list = () => {
@@ -27,16 +27,16 @@ const list = () => {
       <Link href="/">
         <div className={`w-screen flex wrap justify-around text-white bg-[#15202b] flex-wrap hover:bg-slate-700 cursor-pointer`}>home </div>
       </Link>
-      <div className={`${style.wrapper} flex wrap justify-around flex-wrap `}>List
+      <div className={`${style.wrapper} flex wrap justify-around flex-wrap`}>List
         <DynamicCSS />
         <Stack direction="row" spacing={1}>
-      <IconButton aria-label="fingerprint" color="secondary">
-        <Fingerprint />
-      </IconButton>
-      <IconButton aria-label="fingerprint" color="success">
-        <Fingerprint />
-      </IconButton>
-    </Stack>
+          <IconButton aria-label="fingerprint" color="secondary">
+            <Fingerprint />
+          </IconButton>
+          <IconButton aria-label="fingerprint" color="success">
+            <Fingerprint />
+          </IconButton>
+        </Stack>
         <RadioGroup />
         <Switch />
         {/* <img className='flex-wrap w-[250px] h-[200px] mt-[7vh] p-5' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1inLywmsyHAMuV9D1VdKW0PZsx9RawIayulqNLlvl-lYbhc5YLIy-8tsGlN0YQffrFL0&usqp=CAU' /> */}
